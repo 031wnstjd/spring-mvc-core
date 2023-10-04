2898bba1c7cf5d96ffcef7a56fc9931c63b06cc1
Junsung <031wnstjd@naver.com>
2023-10-05 01:07:27 +0900

    상품 등록 처리 - @ModelAttribute

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -36,6 +36,41 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model
+    ) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+//        model.addAttribute("item", item); // @ModelAttribute이 해당 로직을 자동 추가하므로 생략 가능
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) { // @ModelAttribute의 name 속성도 생략할 경우, Item(클래스명) -> item로 name 속성을 자동 지정
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(Item item) { // 객체 or HashMap의 경우, 어노테이션 생략 시 @ModelAttribute 자동 적용
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
